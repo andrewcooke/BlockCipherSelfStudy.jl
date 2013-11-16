eba90c4e7b67e1cf3dc1e258de157597434d4bf8
andrew cooke <andrew@acooke.org>
2013-11-16 16:45:55 -0300

    trivial solution

--- src/RC5.jl
+++ src/RC5.jl
@@ -1,6 +1,6 @@
 
 module RC5
-using Blocks
+using Blocks, Solve, Tasks
 
 const P8 = 0xb7
 const Q8 = 0x9e
@@ -15,16 +15,28 @@ immutable State{W<:Unsigned}
     r::Uint8
     k::Array{Uint8}
     s::Array{W}
+    rotate::Bool
 end
 
-State(w::Type{Uint8}, r::Uint8, k::Array{Uint8}) = State{w}(r, k, expand(w, r, k, P8, Q8))
-State(w::Type{Uint16}, r::Uint8, k::Array{Uint8}) = State{w}(r, k, expand(w, r, k, P16, Q16))
-State(w::Type{Uint32}, r::Uint8, k::Array{Uint8}) = State{w}(r, k, expand(w, r, k, P32, Q32))
-State(w::Type{Uint64}, r::Uint8, k::Array{Uint8}) = State{w}(r, k, expand(w, r, k, P64, Q64))
+State(w::Type{Uint8}, r::Uint8, k::Array{Uint8}; rotate=true) = 
+State{w}(r, k, expand(w, r, k, P8, Q8), rotate)
+
+State(w::Type{Uint16}, r::Uint8, k::Array{Uint8}; rotate=true) = 
+State{w}(r, k, expand(w, r, k, P16, Q16), rotate)
+
+State(w::Type{Uint32}, r::Uint8, k::Array{Uint8}; rotate=true) = 
+State{w}(r, k, expand(w, r, k, P32, Q32), rotate)
+
+State(w::Type{Uint64}, r::Uint8, k::Array{Uint8}; rotate=true) = 
+State{w}(r, k, expand(w, r, k, P64, Q64), rotate)
 
 function Base.show{W}(io::IO, s::State{W})
     print(io, @sprintf("RC5-%d/%d/%d 0x%s", 
                        8*sizeof(W), s.r, length(s.k), bytes2hex(s.k)))
+end
+
+function same_state{W<:Unsigned}(s1::State{W}, s2::State{W})
+    s1.r == s2.r && s1.s == s2.s && s1.rotate == s2.rotate
 end
 
 function rotatel{W<:Unsigned}(x::W, n::Unsigned)
@@ -33,7 +45,7 @@ function rotatel{W<:Unsigned}(x::W, n::Unsigned)
     convert(W, (x << m) | (x >>> (w - m)))
 end
 rotatel{W<:Unsigned}(::Type{W}, x::Integer, n::Integer) = 
-    rotatel(convert(W, x), n)
+rotatel(convert(W, x), n)
 
 function expand{W<:Unsigned}(w::Type{W}, r::Uint8, k::Array{Uint8}, p::W, q::W)
     u = sizeof(w)
@@ -66,29 +78,52 @@ function encrypt{W<:Unsigned}(s::State{W}, a::W, b::W)
     a::W = a + s.s[1]
     b::W = b + s.s[2]
     for i = 1:s.r
-        a = rotatel(W, a $ b, b) + s.s[2i+1]
-        b = rotatel(W, a $ b, a) + s.s[2i+2]
+        a = rotatel(W, a $ b, b)
+        a = a + s.s[2i+1]
+        b = rotatel(W, a $ b, a)
+        b = b + s.s[2i+2]
     end
     a, b
 end
 
 function encrypt{W<:Unsigned}(s::State{W}, plain)
-    unpack(Uint32,
-           Task() do
-               a = nothing
-               for b in pack(Uint32, plain)
-                   if a == nothing
-                       a = b
-                   else
-                       a, b = encrypt(s, a, b)
-                       produce(a)
-                       produce(b)
-                       a = nothing
-                   end
-               end
-           end)
+    e = Task() do
+        a = nothing
+        for b in pack(Uint32, plain)
+            if a == nothing
+                a = b
+            else
+                a, b = encrypt(s, a, b)
+                produce(a)
+                produce(b)
+                a = nothing
+            end
+        end
+    end
+    unpack(Uint32, e)
 end
 
+
+make_keygen(w, r, k; rotate=true) = 
+() -> State(w, r, collect(Uint8, take(k, rands(Uint8))), rotate=rotate)
+
+function make_solve_r0{W<:Unsigned}(::Type{W}, k)
+    n = 8 * sizeof(W)
+    function solve_r0(e)
+        a, b = pack(W, e(take(2*n, constant(0x0))))
+        s = State(W, 0x0, collect(Uint8, take(k, constant(0x0))))
+        s.s[1] = a
+        s.s[2] = b
+        s
+    end
+end
+
+
+function solutions()
+    solve_known_cipher(3, make_solve_r0(Uint32, 0x2), 
+                       make_keygen(Uint32, 0x0, 0x2),
+                       encrypt, eq=same_state)
+end
 
 
 function test_rotatel()
@@ -101,18 +136,21 @@ function test_rotatel()
 end
 
 function test_vectors()
-    a, b = encrypt(State(Uint32, 0x0c, zeros(Uint8, 16)), 0x00000000, 0x00000000)
+    a, b = encrypt(State(Uint32, 0x0c, zeros(Uint8, 16)), 
+                   0x00000000, 0x00000000)
     @assert a == 0xeedba521 a
     @assert b == 0x6d8f4b15 b
     # note that bytes are packed little-endian, while the rc5 paper shows
     # 32bit integers for the half-blocks.
-    c = collect(Uint8, encrypt(State(Uint32, 0x0c, 
-                                     hex2bytes("00000000000000000000000000000000")),
-                               hex2bytes("0000000000000000")))
+    c = collect(Uint8, 
+                encrypt(State(Uint32, 0x0c, 
+                              hex2bytes("00000000000000000000000000000000")),
+                        hex2bytes("0000000000000000")))
     @assert c == hex2bytes("21a5dbee154b8f6d")
-    c = collect(Uint8, encrypt(State(Uint32, 0x0c, 
-                                     hex2bytes("5269f149d41ba0152497574d7f153125")),
-                               hex2bytes("65c178b284d197cc")))
+    c = collect(Uint8, 
+                encrypt(State(Uint32, 0x0c, 
+                              hex2bytes("5269f149d41ba0152497574d7f153125")),
+                        hex2bytes("65c178b284d197cc")))
     @assert c == hex2bytes("eb44e415da319824")
 end
 
@@ -121,8 +159,7 @@ function tests()
     test_vectors()
 end
 
-end
+tests()
+solutions()
 
-RC5.tests()
-println(RC5.State(Uint8, 0x0c, b"secret"))
-println(RC5.State(Uint32, 0x0c, b"1234567812345678"))
+end
