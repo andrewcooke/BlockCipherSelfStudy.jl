7a1f3afb6f6c34aa4795693392d251b41fc1ee88
andrew cooke <andrew@acooke.org>
2013-11-19 00:08:36 -0300

    trying r1 noro

--- src/Experiment.jl
+++ src/Experiment.jl
@@ -8,22 +8,22 @@ function xor_diff_stats()
     # it seems like one way to do that is to choose X randomly, flip
     # a bit for Y, and look at the stats for the difference.
     n = 100
-    b = 4
+    b = 0
     m::Uint8 = 2 ^ b
     results = Array(Uint8, 2, n)
     for j = 1:2
         for i = 1:n
             k::Uint8 = rand(Uint8)
             if j == 1
-                k = k & (~m)
+                k = k & ~m
             else
                 k = k | m
             end
             c::Uint8 = rand(Uint8)
             x::Uint8 = rand(Uint8)
             # xor doesn't work below (symmetric results), but subtraction
-            # or addition do, nicely, except for bit 7
-            y::Uint8 = x + m
+            # works nicely.  addition isn't so good for bit 7 - why?
+            y::Uint8 = x - m
             d::Uint8 = ((x-c) $ k) - ((y-c) $ k)
             println("$x $y $c $k $d")
             results[j, i] = d
