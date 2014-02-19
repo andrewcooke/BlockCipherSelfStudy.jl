8f74faad4bceaa1443150a96c248b3443851d6a0
andrew cooke <andrew@acooke.org>
2014-02-18 21:25:39 -0300

    obscure julia error

--- src/DES.jl
+++ src/DES.jl
@@ -3,6 +3,9 @@ module DES
 using Tasks, Solve
 
 
+# An implementation of DES.  Encryption only, and not very efficient.
+
+
 abstract SBox
 type WithSBox <: SBox end
 type WithoutSBox <: SBox end
@@ -17,6 +20,14 @@ end
 
 State{S<:SBox}(key::Uint64; r::Uint8=0x10, sbox::Type{S}=WithSBox) = 
 State{S}(r, key, expand_key(r, key), sbox)
+
+
+mask(n) = (one(Uint64) << n) - 1
+const MASK_28 = mask(28)
+const MASK_32 = mask(32)
+const MASK_6 = mask(6)
+const MASK_4 = mask(4)
+const MASK_2 = mask(2)
 
 
 const KEY = [57, 49, 41, 33, 25, 17,  9,  1, 58, 50, 42, 34, 26, 18,
@@ -39,12 +50,41 @@ const FINAL = [40,  8, 48, 16, 56, 24, 64, 32, 39,  7, 47, 15, 55, 23, 63, 31,
                36,  4, 44, 12, 52, 20, 60, 28, 35,  3, 43, 11, 51, 19, 59, 27,
                34,  2, 42, 10, 50, 18, 58, 26, 33,  1, 41,  9, 49, 17, 57, 25]
 
-const EXPANSION = [32,  1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,  8,  9, 10, 11, 
-                   12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 
-                   22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,  1]
-
 const PBOX = [16,  7, 20, 21, 29, 12, 28, 17,  1, 15, 23, 26,  5, 18, 31, 10, 
                2,  8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25]
+
+const SBOX = [[[14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7],
+               [ 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8],
+               [ 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0],
+               [15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13]],
+              [[15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10],
+               [ 3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5],
+               [ 0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15],
+               [13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9]],
+              [[10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8],
+               [13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1],
+               [13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7],
+               [ 1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12]],
+              [[ 7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15],
+               [13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9],
+               [10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4],
+               [ 3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14]],
+              [[ 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9],
+               [14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6],
+               [ 4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14],
+               [11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3]],
+              [[12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11],
+               [10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8],
+               [ 9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6],
+               [ 4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13]],
+              [[ 4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1],
+               [13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6],
+               [ 1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2],
+               [ 6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12]],
+              [[13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7],
+               [ 1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2],
+               [ 7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8],
+               [ 2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11]]]
 
 
 function expand_key(r::Uint8, key::Uint64)
@@ -62,11 +102,29 @@ function expand_key(r::Uint8, key::Uint64)
 end
 
 
-function f(r::Uint32, k48::Uint64, sbox)
-    r48::Uint64 = permute_bits(convert(Uint64, r), 32, 48, EXPANSION)
-    r48 = r48 $ k48
-    
-    permute_bits(r, PBOX)
+function f(r::Uint32, k48::Uint64, ::Type{WithSBox})
+    out::Uint32 = zero(Uint32)
+    # circlify to simplify later slicing
+    w::Uint64 = convert(Uint64, r)
+    w = w << 1 | w >> 31 | w << 33
+    for i in 1:8
+        in = MASK_6 & (w $ k48)
+        w, k48 = w >>> 5, k48 >>> 6
+        row, col = MASK_2 & (w << 1 | w >>> 5), MASK_4 & w >>> 1
+        out = (out << 4) | convert(Uint32, SBOX[i][row+1][col+1])
+    end
+    permute_bits(out, PBOX)
+end
+
+function f(r::Uint32, k48::Uint64, ::Type{WithoutSBox})
+    out::Uint32 = zero(Uint32)
+    # shift to simplify loop
+    k48::Uint64 = k48 >>> 1
+    for i in 1:8
+        out = (out << 4) | convert(Uint32, MASK_4 & (k48 $ r))
+        r, k48 = r >>> 4, k48 >>> 6
+    end
+    permute_bits(out, PBOX)
 end
 
 function encrypt{S<:SBox}(s::State{S}, p::Uint64)
@@ -76,7 +134,7 @@ function encrypt{S<:SBox}(s::State{S}, p::Uint64)
         if i > 1
             l, r = r, l
         end
-        l = l $ f(r, s.k[i], i)
+        l = l $ f(r, s.k[i], s.sbox)
     end
     permute_bits(join_32(l, r), FINAL)
 end
@@ -98,10 +156,6 @@ permute_bits{U<:Unsigned}(in::U, from, perm) = permute_bits(in, from, from, perm
 permute_bits{U<:Unsigned}(in::U, perm) = permute_bits(in, 8*sizeof(U), perm)
 
 
-mask(n) = (one(Uint64) << n) - 1
-const MASK_28 = mask(28)
-const MASK_32 = mask(32)
-
 split_28(k::Uint64) = (k >> 28 & MASK_28, k & MASK_28)
 split_32(k::Uint64) = (convert(Uint32, k >> 32 & MASK_32), convert(Uint32, k & MASK_32))
  
@@ -116,6 +170,9 @@ function test_encrypt()
     for r1 in take(10, rands(Uint64))
         @assert r1 == encrypt(z, r1)
     end
+    # http://cryptomanager.com/tv.html
+    s = State(0x752878397493CB70)
+    @assert encrypt(s, 0x1122334455667788) == 0xB5219EE81AA7499D
 end
 
 function test_permutation()
